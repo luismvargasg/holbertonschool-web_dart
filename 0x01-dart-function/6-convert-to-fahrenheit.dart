3ab02b6c22387d103dcb76cf1ecc62d55cdc6935
luismvargasg <1610@holbertonschool.com>
2022-07-19 10:38:08 -0500

    fixing decimals

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -3,6 +3,6 @@ List<String> convertToF(List<double> tempInC) {
     var idx = 0;
     List<String> arr = [];
 
-    for (; idx < tempInC.length; idx++) { arr.add((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2)); }
+    for (; idx < tempInC.length; idx++) { arr.add(num.parse((tempInC[idx] * 9 / 5 + 32).toStringAsFixed(2))); }
     return arr;
 }
